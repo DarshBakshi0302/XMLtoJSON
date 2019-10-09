1e6c2ec739f03e797f1e593f47918c2a5b7b7ec0
DarshBakshi0302 <54886193+DarshBakshi0302@users.noreply.github.com>
2019-10-09 15:57:47 +0530

    Add files via upload

---
+++ ExampleXMLtoJSON.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install xmltodict
+
+
+# In[1]:
+
+
+import xmltodict, json
+
+
+# In[3]:
+
+
+info=xmltodict.parse('''<breakfast_menu>
+<food>
+<name>Belgian Waffles</name>
+<price>$5.95</price>
+<description>
+Two of our famous Belgian Waffles with plenty of real maple syrup
+</description>
+<calories>650</calories>
+</food>
+<food>
+<name>Strawberry Belgian Waffles</name>
+<price>$7.95</price>
+<description>
+Light Belgian waffles covered with strawberries and whipped cream
+</description>
+<calories>900</calories>
+</food>
+<food>
+<name>Berry-Berry Belgian Waffles</name>
+<price>$8.95</price>
+<description>
+Light Belgian waffles covered with an assortment of fresh berries and whipped cream
+</description>
+<calories>900</calories>
+</food>
+<food>
+<name>French Toast</name>
+<price>$4.50</price>
+<description>
+Thick slices made from our homemade sourdough bread
+</description>
+<calories>600</calories>
+</food>
+<food>
+<name>Homestyle Breakfast</name>
+<price>$6.95</price>
+<description>
+Two eggs, bacon or sausage, toast, and our ever-popular hash browns
+</description>
+<calories>950</calories>
+</food>
+</breakfast_menu>''')
+dump=json.dumps(info)
+print(dump)
+
+
+# In[ ]:
+
+
+
+
